76d8f0e174e648e67736a554d808b67faf333ba1
Ruan Souza <ruan2014souza17@gmail.com>
2023-10-25 09:38:32 -0300

    Adicionando Delete aos vendedores

--- VendodoresMVC/Controllers/SellersController.cs
+++ VendodoresMVC/Controllers/SellersController.cs
@@ -19,7 +19,7 @@ namespace VendedoresMVC.Controllers
             _sellerService = sellerService;
             _departamentService = departamentService;
         }
-        
+
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
@@ -38,6 +38,31 @@ namespace VendedoresMVC.Controllers
         {
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
-        } 
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
